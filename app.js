835a0762ecfa08463624eeff6693760ef6c2bf79
ashok_sriramadasu <ramadasuashok@gmail.com>
2018-06-12 19:03:02 +0530

    changes added

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Dependencies
 var express = require('express'),
-    bodyParser = require('body-parser'),
-    request = require('request'),
-    jwt = require('jsonwebtoken'),
-    OpenTok = require('opentok');
+  bodyParser = require('body-parser'),
+  request = require('request'),
+  jwt = require('jsonwebtoken'),
+  OpenTok = require('opentok');
 
 // Verify that the API Key and API Secret are defined
 const apiKey = process.env.TOKBOX_API_KEY;
 const apiSecret = process.env.TOKBOX_SECRET;
-const port  = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const accessKey = process.env.ACCESS_KEY;
 const secret = process.env.SECRET;
 const certificate = process.env.CERTIFICATE;
@@ -34,6 +34,39 @@ opentok.createSession({ mediaMode: 'routed' }, function (err, session) {
   if (err) throw err;
   app.set('sessionId', session.sessionId);
   token = opentok.generateToken(session.sessionId, { role: 'moderator' });
+  let data = {
+    "type": "s3",
+    "config": {
+      "bucket": bucket,
+      "secretKey": secret,
+      "accessKey": accessKey
+    },
+    "certificate": certificate
+  }
+
+  var currentTime = Math.floor(new Date() / 1000);
+  let key = jwt.sign({
+    "iss": apiKey,
+    "iat": currentTime,
+    "exp": currentTime + 180,
+    "ist": "project",
+    "jti": "jwt_nonce"
+  },
+    apiSecret,
+    {
+      algorithm: 'HS256'
+    })
+  request({
+    url: uri,
+    method: 'PUT',
+    headers: { 'X-OPENTOK-AUTH': key },
+    json: data
+  }, function (err, data) {
+    if (err) {
+      'Could not start archive for session ' + sessionId + '. error=' + err.message
+    }
+  });
+  console.log('data details ' + JSON.stringify(data, null, 2));
   // We will wait on starting the app until this is done
   init();
 });
@@ -107,56 +140,22 @@ app.get('/download/:archiveId', function (req, res) {
 app.post('/start', function (req, res) {
   console.log(req.body);
   let uri = 'https://api.opentok.com/v2/project/' + apiKey + '/archive/storage';
-  var hasAudio = (req.param('hasAudio') !== undefined);
-  var hasVideo = (req.param('hasVideo') !== undefined);
-  var outputMode = req.param('outputMode');
-  let data = {
-    "type": "s3",
-    "config": {
-      "bucket": bucket,
-      "secretKey": accessKey,
-      "accessKey": secret
-    },
-    "fallback": "none",
-    "certificate": certificate
-  }
+  var hasAudio = req.body.hasAudio
+  var hasVideo = req.body.hasVideo;
+  var outputMode = req.body.outputMode;
 
-  var currentTime = Math.floor(new Date() / 1000);
-  let key = jwt.sign({
-    "iss": apiKey,
-    "iat": currentTime,
-    "exp": currentTime + 180,
-    "ist": "project",
-    "jti": "jwt_nonce"
-  },
-    apiSecret,
-    {
-      algorithm: 'HS256'
-    })
-  console.log(data);
-  request({
-    url: uri,
-    method: 'PUT',
-    headers: { 'X-OPENTOK-AUTH': key },
-    json: data
-  }, function (err, data) {
+  // res.json(archive);
+  opentok.startArchive(app.get('sessionId'), {
+    name: 'Node Opentok Encryption App',
+    hasAudio: hasAudio,
+    hasVideo: hasVideo,
+    outputMode: outputMode
+  }, function (err, archive) {
     if (err) return res.send(500,
       'Could not start archive for session ' + sessionId + '. error=' + err.message
     );
-    console.log('data details ' + JSON.stringify(data, null, 2));
-    // res.json(archive);
-    opentok.startArchive(app.get('sessionId'), {
-      name: 'Node Opentok Encryption App',
-      hasAudio: true,
-      hasVideo: true,
-      outputMode: 'composed'
-    }, function (err, archive) {
-      if (err) return res.send(500,
-        'Could not start archive for session ' + sessionId + '. error=' + err.message
-      );
-      console.log('archive details ' + JSON.stringify(archive, null, 2));
-      res.json(archive);
-    });
+    console.log('archive details ' + JSON.stringify(archive, null, 2));
+    res.json(archive);
   });
 });
 
